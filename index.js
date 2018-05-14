e96ae9cf8548ce8f78ca44a0e1e9cb8991d1b4c7
aleguern <36839434+aleguern@users.noreply.github.com>
2018-05-14 09:39:50 +0200

    both joystick test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 var express = require('express'); //require express
 var app = express();
-//var server = app.listen(8080); //on computer
-var server = app.listen(788); //on BB
+var server = app.listen(8080); //on computer
+//var server = app.listen(788); //on BB
 var io = require('socket.io').listen(server);
 var bodyParser = require('body-parser'); //require bodyParser
 const isNumber = require('is-number'); //number
@@ -35,13 +35,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.get('/timelapse',function(req,res){
-  //res.sendFile("/Users/antoineleguern/Documents/rail/webServer-master/index.html");
-  res.sendFile("/root/webserver/index.html"); //to read on BB
+  res.sendFile("/Users/antoineleguern/Documents/rail/webServer-master/index.html");
+  //res.sendFile("/root/webserver/index.html"); //to read on BB
 });
 
 app.get('/nipples',function(req,res){
-  //res.sendFile("/Users/antoineleguern/Documents/rail/webServer-master/test/index1.html");
-  res.sendFile("/root/webserver/test/index1.html"); //to read on BB
+  res.sendFile("/Users/antoineleguern/Documents/rail/webServer-master/test/index1.html");
+  //res.sendFile("/root/webserver/test/index1.html"); //to read on BB
 });
 
 app.post('/timelapse/login',function (req, res) {
@@ -88,26 +88,8 @@ io.on('connection', function(socket){
     if(typeof angle !== 'undefined') {
 
       mode = "w"; //writing
-/*
-      if(dir == "left"){
-        write = "-1 "+ number;
-        file = "moteur2.txt";
-      }
-      else if(dir == "right"){
-        write = "+1 "+ number;
-        file = "moteur2.txt";
-      }
-      else if(dir == "down") {
-        write = "-1 "+ number;
-        file = "moteur3.txt";
-      }
-      else if(dir == "up"){
-        write = "+1 "+ number;
-        file = "moteur3.txt";
-      }
-      else console.log("error");*/
 
-      if(angle <= 30 && angle >= 330){
+      if(angle >= 0 && angle <= 30 || angle >= 330 && angle <= 360){
         write = "+"+ number;
         fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
       }
@@ -144,7 +126,7 @@ io.on('connection', function(socket){
         write = "-"+ number;
         fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
       }
-      else if(angle >= 300 && angle <= 330) {
+      else if(angle > 300 && angle < 330) {
         write = "-"+ number;
         fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
 
@@ -154,6 +136,95 @@ io.on('connection', function(socket){
 
       else {
         console.log("error");
+        console.log(angle);
+      };
+    }
+
+    //écriture dans un fichier en py
+    /*if(isNumber(msg)) python(String(Math.trunc(msg*100)),0,0,0);
+    else python(msg,0,0,0);*/
+
+  });
+});
+
+
+io.on('connection', function(socket){
+  socket.on('chat message2', function(msg){
+    io.emit('chat message2', msg);
+
+    console.log(msg);
+
+/*
+
+    //console.log(msg);
+    if(msg < 4) {
+      if(precisionRound((msg*1000)/2000, 1) > 1.5) number = "1.5"
+      else number = String(precisionRound((msg*1000)/2000, 1));
+      //force = msg;
+      //console.log("force : " + msg)
+    }
+    else if (msg >= 4) angle = msg;
+    //else console.log("direction : " + msg);
+
+    //écriture dans un fichier en node
+    /*
+    if(isNumber(msg)) {
+
+    }
+    else dir = msg;
+////////
+    if(typeof angle !== 'undefined') {
+
+      mode = "w"; //writing
+
+      if(angle >= 0 && angle <= 30 || angle >= 330 && angle <= 360){
+        write = "+"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+      }
+      else if(angle > 30 && angle < 60){
+        write = "+"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+
+        write = "+"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+      }
+      else if(angle >= 60 && angle <= 120){
+        write = "+"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+      }
+      else if(angle > 120 && angle < 150){
+        write = "+"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+
+        write = "-"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+      }
+      else if(angle >= 150 && angle <= 210){
+        write = "-"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+      }
+      else if(angle > 210 && angle < 240){
+        write = "-"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+
+        write = "+"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+      }
+      else if(angle >= 240 && angle <= 300) {
+        write = "-"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+      }
+      else if(angle > 300 && angle < 330) {
+        write = "-"+ number;
+        fs.writeFileSync("moteur3.txt", write, "UTF-8",mode);
+
+        write = "+"+ number;
+        fs.writeFileSync("moteur2.txt", write, "UTF-8",mode);
+      }
+
+      else {
+        console.log("error");
+        console.log(angle);
       };
     }
 
