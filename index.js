43448623bc3edc1138354725920109535eede649
aleguern <36839434+aleguern@users.noreply.github.com>
2018-05-15 11:40:33 +0200

    Added start and stop

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ io.on('connection', function(socket){
 
     if(msg == "forward") fs.writeFileSync("moteur1.txt", "1", "UTF-8", "w");
     else if(msg == "backward") fs.writeFileSync("moteur1.txt", "-1", "UTF-8","w");
+    else if(msg == "start") fs.writeFileSync("mode.txt", "1", "UTF-8","w");
+    else if(msg == "stop") fs.writeFileSync("mode.txt", "0", "UTF-8","w");
 
     if(typeof dir !== 'undefined') {
 
@@ -68,7 +70,7 @@ io.on('connection', function(socket){
       else if(dir == "up"){
         fs.writeFileSync("moteur3.txt","+"+ number, "UTF-8","w");
         }
-      else console.log("error");
+      else console.log();
 
     }
 
